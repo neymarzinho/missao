f0ac8632863bb737f8105d7567ecf8530c32d69f
neymarzinho <128197097+neymarzinho@users.noreply.github.com>
2024-09-20 21:37:02 -0300

    .

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ function mostraResultado() {
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
-    botaoJogarNovamente.addEventListener("click", jogaNovamente() )
+    botaoJogarNovamente.addEventListener("click", jogaNovamente())
 }
 
 function jogaNovamente () {
