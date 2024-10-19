f22d8ffbc67a19b086ecb2f3103dd3ea399e82b3
neymarzinho <128197097+neymarzinho@users.noreply.github.com>
2024-10-18 22:17:16 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import {aleatorio} from './aleatorio.js';
+import {aleatorio, nome} from './aleatorio.js';
 import {perguntas} from './perguntas.js';
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
@@ -41,7 +41,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, ${nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar");
@@ -54,4 +54,11 @@ function jogaNovamente(){
     caixaResultado.classList.remove("mostrar");
     mostraPergunta();
 }
+
+function substituiNome(){
+    for(const pergunta of perguntas){
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+    }
+}
+substituiNome();
 mostraPergunta();
